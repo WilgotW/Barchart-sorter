2b570e7202168a1a1de657514bc3e07e5a26b25a
Wilgot <wilgotwardell@hotmail.com>
2022-09-13 20:42:51 +0200

    cleanup

--- src/components/SorterWindow.jsx
+++ src/components/SorterWindow.jsx
@@ -7,12 +7,12 @@ function SorterWindow() {
     const [barIndex, setBarIndex] = useState(0);
     const [leftBarIndex, setLeftBarIndex] = useState(0);
     
-    // let barIndex;
+    
     let iterations = 0;
 
     const randomNum = (max, min) => Math.floor(Math.random() * (max - min) + min); 
 
-    const addBar = bar => {        
+    const addBar = () => {        
         const newBar = {
             height: randomNum(20, 500),
             background: "orange"
@@ -24,23 +24,13 @@ function SorterWindow() {
         setLeftBarIndex(bars.length - 1);
     }
 
-    useEffect(() => {
-
-    }, [])
-
     function sort () {
-        // console.log(bars.length);
         const allBars = [...bars];
-        
-        // console.log("checking " + barIndex + " to " + leftBarIndex);
-
         //select bar
         const selectedBar = { ...allBars[barIndex] };
-        //mark it with color blue
-        selectedBar.background = "blue";
-    
-        //get bar to the left:
         const leftBar = { ...allBars[leftBarIndex] };
+        //mark it with color
+        selectedBar.background = "blue";
         leftBar.background = "green";
         
         allBars[barIndex] = selectedBar;
@@ -49,8 +39,6 @@ function SorterWindow() {
         if(selectedBar.height < leftBar.height){
             swapItems(allBars, barIndex, leftBarIndex);
         }
-
-        // setBars(allBars);
 
         if(leftBarIndex != 0){
             setBarIndex(barIndex - 1);
@@ -63,10 +51,6 @@ function SorterWindow() {
         }
         setBars(allBars);
     }
-   
-    // function callSort(){
-        
-    // }
 
     const swapItems = (arr, item1, item2) => {
         const temp = arr[item1];
@@ -74,7 +58,6 @@ function SorterWindow() {
         arr[item2] = temp;
     }
     
-
   return (
     <div>
         <button onClick={addBar}>Add</button>
